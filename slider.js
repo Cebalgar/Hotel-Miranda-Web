f65d5617b376fcf9b663d44ef41290979a02ac0b
Celia Ballesteros <cebalgar@gmail.com>
2022-12-20 13:47:42 +0100

    web-destop

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
-
-const swiper = new Swiper(".swiper", {
+// SLIDER ROOMS
+const swiper = new Swiper(".rooms__slider", {
     direction: "horizontal",
     loop: true,
     slidesPerView: 1,
@@ -20,6 +20,73 @@ const swiper = new Swiper(".swiper", {
   });
   
 
-  
-  
+  //SLIDER FACILITES
+  const initSwiperFacilities = function () {
+    const swiper2 = new Swiper(".facilities__slider", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  };
+//SLIDER MENU
+const initSwiperMenu = function () {
+  const swiper3 = new Swiper(".menu__slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+};
+
+//PAGINATION MENU
+const initPaginationMenu = function () {
+  const pagination = new Swiper(".menu__pagination", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+};
+
+//INIT SWIPERS
+const mql = window.matchMedia("(max-width: 899px)");
+const mql2 = window.matchMedia("(max-width: 799px)");
+if (innerWidth < 900) {
+  initSwiperFacilities();
+  initSwiperMenu();
+}
+if (innerWidth < 800) {
+  initPaginationMenu();
+}
+mql.onchange = (e) => {
+  if (e.matches) {
+    initSwiperFacilities();
+    initSwiperMenu();
+  } else {
+    location.reload();
+  }
+};
+mql2.onchange = (e) => {
+  if (e.matches) {
+    initPaginationMenu();
+  } else {
+    location.reload();
+  }
+};
+
  
